3fad3b7d1960bee8aac60e01e6aff0e71d3bb20f
Kevin Hu <yhu104@aucklanduni.ac.nz>
2022-08-09 13:50:21 +1200

    Add text display for api response

--- 1. Frontend/msa-phase2-frontend/src/App.tsx
+++ 1. Frontend/msa-phase2-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { CurrentResponse } from 'openweathermap-ts/dist/types';
 import React, { useState } from 'react';
 import './App.css';
 
@@ -6,8 +7,14 @@ const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
 
 function App() {
   const [input, setInput] = useState("");
-  const [cityName, setCityName] = useState<string>();
-  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const [searched, setSearched] = useState<boolean>(false);
+  const [weather, setWeather] = useState<CurrentResponse | undefined>(undefined);
+
+  const search = () => {
+    axios.get(`${WEATHER_BASE_URL}${input}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`).then(res => setWeather(res.data));
+    
+    setSearched(true);
+  };
 
   return (
     <div>
@@ -18,16 +25,21 @@ function App() {
       <div>
         <label>City Name</label><br/>
         <input type="text" id="city-name" name="city-name" onChange={e => setInput(e.target.value)}/><br/>
-        <button onClick={() => {
-          setCityName(input);
-          console.log(`${WEATHER_BASE_URL}${input}&appid=${apiKey}`);
-          axios.get(`${WEATHER_BASE_URL}${input}&appid=${apiKey}`).then(res => console.log(res));
-        }}>
+        <button type="submit" onClick={search}>
         Search
         </button>
       </div>
-
-      {cityName}
+      {weather === undefined ? 
+        (searched ? 
+          <p>Search failed. Maybe try another name?</p> : 
+          <p>Try out Auckland or hamilton,nz to start with :)</p>
+        ) : <div>
+          {weather.name}, {weather.sys.country}
+          <br/>
+          Humidity: {weather.main.humidity}%
+        </div>
+      }
+      {searched}
     </div>
   );
 }
